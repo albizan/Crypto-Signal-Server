151196b9b59cb83874d65ff15c4553c8222f89e1
Alberto Zanotti <albizan@hotmail.it>
2018-06-16 18:45:34 +0200

    Added getSpecificSignal function in signal controller

--- controllers/signal.controller.js
+++ controllers/signal.controller.js
@@ -1,20 +1,36 @@
 import Signal from '../models/signal.model';
 
 export default {
+
+  // Get all the cryptocurrency signals found in the mongo database
   getAllSignals: (req, res) => {
     Signal.find({})
       .then(foundData => res.status(201).json(foundData))
       .catch(err => res.status(500).json(err));
   },
+
+  // Delete all the cryptocurrency signals found in the mongo database
+  // If the operation is succesful, return the number of documents removed
   deleteAllSignals: (req, res) => {
     Signal.remove({})
-      .then(obj => res.status(200).json(obj.n))
+      .then(obj => res.status(201).json(obj.n))
       .catch(err => res.status(500).json(err));
   },
+
+  // Receive new signal object from frontend, insert it in the mongo collection
+  // Signal object are validated in the frontend
   newSignal: (req, res) => {
-    // Saving received data in the mongo database
     Signal.create(req.body)
       .then(savedData => res.status(201).json(savedData))
       .catch(err => res.status(500).json(err));
+  },
+
+  // Retreive a specific signal from the database, send it to frontend
+  getSpecificSignal: (req, res) => {
+    const signalID = req.params.id;
+    console.log(signalID);
+    Signal.findById(signalID)
+      .then(retreivedSignal => res.status(201).json(retreivedSignal))
+      .catch(err => res.status(504).json(err));
   },
 };
