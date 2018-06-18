f004cbb9a59a83e77621215814e8dc6580106c0b
Alberto Zanotti <albizan@hotmail.it>
2018-06-18 15:06:38 +0200

    Updated .gitignore

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const configData = {
+  port: process.env.port || 5555,
+  dbUser: process.env.dbUser,
+  dbPassword: process.env.dbPassword,
+  dbURI: process.env.dbURI,
+};
+
+export default configData;
